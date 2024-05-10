a7e244010452ba37c8e48127a9d7faa0701b22b6
Rafael Ribeiro <cortadordeunha666@gmail.com>
2024-05-10 19:22:23 -0300

    feat: fix snake can instantly reverse its direcetion and kill snake when it hits itself.

--- game.js
+++ game.js
@@ -4,25 +4,36 @@ const canvasContext = canvasEL.getContext("2d")
 
 let snakePositions = [{x:3, y: 5}, {x:2, y: 5}, {x:1, y: 5}]
 let foodPosition = {}
-let directionX = 1
-let directionY = 0
 let points = 0
 const pixelSize = 20
+
+let directionX = 1
+let directionY = 0
+const nextDirection = {x: 1, y: 0}    // this will be used to store the final snake position, preventing that it moves to the opposite direction immediatly
+
 let gameInterval = null
 let gameMaxNumberOfColumns = canvasEL.width / pixelSize
 let gameMaxNumberOfRows = canvasEL.height / pixelSize
 
 
+function updateSnakeDirection() {
+  directionX = nextDirection.x
+  directionY = nextDirection.y
+}
+
 function snakeShouldDie(headPosition) {
   if (headPosition.x === -1 || headPosition.y === -1) {   // snake hitted the top or the left of the canvas
-    clearInterval(gameInterval)
     return true
   }
 
-  else if (headPosition.x === gameMaxNumberOfColumns || headPosition.y === gameMaxNumberOfRows) {
-    clearInterval(gameInterval)
+  else if (headPosition.x === gameMaxNumberOfColumns || headPosition.y === gameMaxNumberOfRows) {   // snake hitted the right or the bottom of the canvas
     return true
   }
+
+  const snakePositionsWithoutHead = snakePositions.slice(1)
+  const snakeHittedItself = snakePositionsWithoutHead.some(({x, y}) => headPosition.x === x && headPosition.y === y)
+
+  if (snakeHittedItself) return true
 }
 
 function updateScore() {
@@ -46,20 +57,26 @@ function moveSnake() {
   const snakeHead = {...snakePositions[0]}
   const snakeTail = snakePositions[snakePositions.length -1]
 
+  updateSnakeDirection()
+
   snakePositions.unshift({x: snakeHead.x + directionX, y: snakeHead.y + directionY})   // push snake 1 pixel forward
-
-  if (snakeShouldDie(snakePositions[0])) return
-
-  snakePositions.forEach(({x, y}) => {    // paint whole snake
-    canvasContext.fillStyle = "yellow"
-    canvasContext.fillRect(x * pixelSize, y * pixelSize, pixelSize, pixelSize)
-  })
 
   if (!snakeAteTheFood(snakePositions[0])) {
     canvasContext.clearRect(snakeTail.x * pixelSize, snakeTail.y * pixelSize, pixelSize, pixelSize)   // remove snake tail from canvas
   
     snakePositions.pop()  // remove tail
   }
+
+  if (snakeShouldDie(snakePositions[0])) {
+    clearInterval(gameInterval)
+    return
+  }
+
+  snakePositions.forEach(({x, y}, index) => {    // paint whole snake
+    index ? canvasContext.fillStyle = "yellow" : canvasContext.fillStyle = "rgb(255, 215, 0)"
+    
+    canvasContext.fillRect(x * pixelSize, y * pixelSize, pixelSize, pixelSize)
+  })
 
 }
 
@@ -68,14 +85,12 @@ function generateFood() {
     const randomPositionX = Math.floor(Math.random() * (gameMaxNumberOfColumns - 1))
     const randomPositionY = Math.floor(Math.random() * (gameMaxNumberOfRows - 1))
 
-    console.log(randomPositionX, randomPositionY);
-
     const generatedPositionIsTaken = snakePositions.some(partPosition => partPosition.x === randomPositionX && partPosition.y === randomPositionY)
 
     if (!generatedPositionIsTaken) {
       foodPosition = {x: randomPositionX, y: randomPositionY}
 
-      canvasContext.fillStyle = "purple"
+      canvasContext.fillStyle = "gray"
       canvasContext.fillRect(randomPositionX * pixelSize, randomPositionY * pixelSize, pixelSize, pixelSize)
 
       break
@@ -87,20 +102,20 @@ function changeSnakeDirection(event) {
   const pressedKey = event.key
 
   if (pressedKey === "ArrowUp" && directionY === 0) {
-    directionX = 0
-    directionY = -1
+    nextDirection.x = 0
+    nextDirection.y = -1
   }
   else if (pressedKey === "ArrowLeft" && directionX === 0) {
-    directionX = -1
-    directionY = 0
+    nextDirection.x = -1
+    nextDirection.y = 0
   }
   else if (pressedKey === "ArrowDown" && directionY === 0) {
-    directionX = 0
-    directionY = 1
+    nextDirection.x = 0
+    nextDirection.y = 1
   }
   else if (pressedKey === "ArrowRight" && directionX === 0) {
-    directionX = 1
-    directionY = 0
+    nextDirection.x = 1
+    nextDirection.y = 0
   }
 }
 
